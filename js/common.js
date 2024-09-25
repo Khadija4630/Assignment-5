d74c6e8b55238a07b3d09aafab27fbd306e18b11
Khadija4630 <khadijahaque085@gmail.com>
2024-09-24 23:49:58 -0400

    common javascript

---
+++ js/common.js
@@ -0,0 +1,55 @@
+// Common functions
+
+
+const modal =document.getElementById('modal');
+const button1 = document.getElementById('btn-noakhali');
+const button2 = document.getElementById('btn-feni');
+const button3 = document.getElementById('btn-quota');
+
+const btnClose = document.getElementById('btn-close');
+
+button1.addEventListener('click',function(){
+   modal.showModal();
+
+});
+button2.addEventListener('click',function(){
+   modal.showModal();
+});
+button3.addEventListener('click',function(){
+   modal.showModal();
+});
+
+btnClose.addEventListener('click',function(){
+   modal.close();
+});
+
+
+
+function getInputFieldValueById(id){
+    const inputValue= document.getElementById(id).value;
+    const inputNumber = parseFloat(inputValue);
+    return inputNumber;
+};
+
+function getTextFieldValueById(id){
+    const amountValue = document.getElementById(id).innerText;
+    const amountNumber = parseFloat(amountValue);
+    return amountNumber;
+}
+
+
+function showSectionById(id){
+    document.getElementById('donation-section').classList.add('hidden');
+    document.getElementById('transaction-section').classList.add('hidden');
+
+    document.getElementById(id).classList.remove('hidden');
+    
+};
+
+
+
+// function toggleActiveButton(id) {
+//    document.getElementById('show-donation').classList.remove('active');
+//    document.getElementById('show-history').classList.remove('active');
+//    document.getElementById(id).classList.add('active');
+// };
--- js/donate.js
+++ js/donate.js
@@ -1,85 +1,18 @@
-// const donationButton = document.getElementById('show-donation-option');
-// const historyButton = document.getElementById('show-transaction-history');
 
 
-document.getElementById('btn-noakhali')
-.addEventListener('click',function(event){
-    event.preventDefault();
-    const addMoney = document.getElementById('noakhali-input').value;
-    const addMoneyAmount = parseFloat(addMoney);
-    const balance =document.getElementById('noakhali-amount').innerText;
-    
-    console.log(addMoney);
-    if(addMoney > 0 || balance < addMoney){
-        const balanceAmount =parseFloat(balance);
-        const newBalance =balanceAmount + addMoneyAmount;
-
-        document.getElementById('noakhali-amount').innerText =newBalance;
-    }
-    else{
-        alert('Invalid Input');
-    }
-});
-// document.getElementById('btn')
-// .addEventListener('click', function(event){
-//     event.preventDefault();
-
-//     const addMoney = getInputFieldValueById('donation-noakhali');
-
-
-//        if(addMoney>0){
-
-      
-//         const balance =getTextFieldValueById('account-balance');
-//         const newBalance= balance +addMoney;
-//         console.log("i know");
-//         document.getElementById('account-balance').innerText =newBalance;
-        
-//         const p=document.createElement('p');
-//         p.innerText= ` Added: ${addMoney} Tk . New Balance: ${newBalance} `;
-//         console.log('p');
-
-
-//         // should be common function
-
-//         document.getElementById('transaction-container').appendChild(p);
-//     }
-
-
-//     else{
-//         alert('failed to add money');
-//     }
+// document.getElementById('show-donation').addEventListener('click', function() {
+  
+//     toggleActiveButton('show-donation');
 // });
 
-
-// // Transaction-history
-
-// document.getElementById('show-donation-option')
-// .addEventListener('click' ,function(){
-
-//     showSectionById('donation-section');
-//     console.log("i know");
-
-// });
-
-// document.getElementById('show-transaction-history')
-// .addEventListener('click', function(){
-//     showSectionById('transaction-section');
-//     console.log("i know");
+// document.getElementById('show-history').addEventListener('click', function() {
+   
+//     toggleActiveButton('show-history');
 // });
 
 
 
-// function showSectionById (id){
-//     // hide all the sections
-//     document.getElementById('donation-section').classList.add('hidden');
-//     document.getElementById('transaction-section').classList.add('hidden');
-//     // show the section with the provide id as parameter
 
-//     document.getElementById(id).classList.remove('hidden');
-    
-
-// };
 
 
 
